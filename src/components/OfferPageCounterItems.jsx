ae9e72b73de805e8a7ec9e0f50f15c1d6c9cf891
Andres Diaz <ivanandresdilox@gmail.com>
2021-04-17 16:07:09 -0500

    Scroll agregado a los productos del store pantaloes Oxford

---
+++ src/components/OfferPageCounterItems.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/OfferPageCounterItems.scss';
+
+const OfferPageCounterItems = () => {
+  return (
+    <div className='OfferPage__counter--container'>
+      <div className='OfferPage__counter' />
+      <div className='OfferPage__counter' />
+      <div className='OfferPage__counter--blue' />
+      <div className='OfferPage__counter' />
+      <div className='OfferPage__counter' />
+      <div className='OfferPage__counter' />
+      <div className='OfferPage__counter' />
+      <div className='OfferPage__counter' />
+      <div className='OfferPage__counter' />
+      <div className='OfferPage__counter' />
+      <div className='OfferPage__counter' />
+      <div className='OfferPage__counter' />
+      <div className='OfferPage__counter' />
+      <div className='OfferPage__counter' />
+      <div className='OfferPage__counter' />
+    </div>
+  );
+};
+export default OfferPageCounterItems;
